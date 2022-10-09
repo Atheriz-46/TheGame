10681b4a480247bd1a4591ac46cd01fc474fe6f2
Aditya <49459489+Atheriz-46@users.noreply.github.com>
2022-10-09 17:06:10 +0530

    Updated Overall State

--- src/overallState.py
+++ src/overallState.py
@@ -2,16 +2,20 @@
 
 import json
 
+from src.playerState import PlayerState
+
 class OverallState:
     def __init__(self,gameMode):
         self.players = []
         self.balloons = []
         self.gm = gameMode
     
-    def addPlayer(self,player):
+    def createPlayer(self):
         if len(self.players)+1 > self.gm.nplayers:
             raise Exception(f"Lobby Full. Lobby limit is {self.gm.nplayers}")
+        player = PlayerState()
         self.players.append(player)
+        return player
         
     def getState(self):
         return {'players':[x.getState() for x in self.players], 'balloons': [x.getState() for x in self.balloons]}
