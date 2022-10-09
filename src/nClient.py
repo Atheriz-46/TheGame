8871f72134e08cfa42e24b1139905ca61c1fabbc
HrshWardhan <hwardhan337@gmail.com>
2022-10-09 14:13:30 +0530

    client added

--- src/nClient.py
+++ src/nClient.py
@@ -4,19 +4,34 @@ import re
 import argparse
 import time
 import sys
+from constants import * 
 
 class NetworkClient:
-    def register(self,,uid):
-        sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        reciever = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sender.connect((self.server_ip,self.server_port))
-        t1 = threading.Thread(target=send,args=(sender,uid))
-        t2 = threading.Thread(target=recieve,args=(reciever,sender,uid))
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
+    
+    def send(self):
+        while True:
+            time.sleep(STATE_SYNC_LATENCY)
+            self.sender.send(m.encode(self.parent.getState()))
+    
+    def recieve(self):
+        while True:
+            message = self.reciever.recv(STATE_MESSAGE_SIZE).decode()
+            parent.setState(message)
+    
+    def register(self):
+        self.sender   = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sender.connect((self.server_ip,self.server_port)) 
+        self.reciever.connect((server_ip, PORT))  
+        recvThread = threading.Thread(target=self.recieve)
+        sendThread = threading.Thread(target=self.send)
+        recvThread.start()
+        sendThread.start()
+        recvThread.join()
+        sendThread.join()
 
-    def __init__(self,server_ip,server_port):
+    def __init__(self,server_ip,server_port,parent):
         self.server_ip = server_ip
         self.server_port = server_port
+        self.parent = parent
+        register()
