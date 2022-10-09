1541062d0acbd84e88d4f3cae6a36ff09584e64e
Aditya <49459489+Atheriz-46@users.noreply.github.com>
2022-10-09 16:55:10 +0530

    Interface for Client and server application layer.

---
+++ src/Client.py
@@ -0,0 +1,7 @@
+class Client:
+    def shoot(self):
+        pass
+    def rotateClock(self):
+        pass
+    def rotateAntiClock(self):
+        pass
---
+++ src/Server.py
@@ -0,0 +1,4 @@
+class Server:
+    def mergeState(self):
+        pass
+    
