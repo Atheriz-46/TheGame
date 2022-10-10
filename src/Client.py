ad6ce7d2669b843c4dd0e8aa1bfd97a4ed20ddf2
HrshWardhan <hwardhan337@gmail.com>
2022-10-11 03:29:15 +0530

    Client finished

--- src/Client.py
+++ src/Client.py
@@ -1,12 +1,24 @@
-from tick import *
+from tick               import *
+from src.nClient        import NetworkClient
+from src.overallState   import OverallState
+from src.gameMode       import GameMode
 
 class Client:
 
-    def __init__(self):
-        self.eventQueue = []
-        
+    def __init__(self,server_ip,server_port,velocityBullets,regenBullets,maxBullets,balloonSeed,balloonCount):
+        self.eventQueue     = []
+        self.networkManager = NetworkClient(server_ip,server_port,self)
+        self.state          = OverallState(GameMode(regenBullets, maxBullets, balloonSeed, balloonCount))
+ 
     def rotateClock(self):
         self.eventQueue.append([time(),'C'])
     
     def rotateAntiClock(self):
         self.eventQueue.append([time(),'A'])
+
+    def getState():
+        # TODO clear Queue after doing this 
+        return {x.getState() for x in self.eventQueue}
+
+    def setState(self,state):
+        self.OverallState.setState(state)
