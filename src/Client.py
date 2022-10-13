e114d99ba15ecd68d00cdc4c048a96787350f6ef
HrshWardhan <hwardhan337@gmail.com>
2022-10-13 16:45:19 +0530

    client thread safety added

--- src/Client.py
+++ src/Client.py
@@ -2,23 +2,43 @@ from tick               import *
 from src.nClient        import NetworkClient
 from src.overallState   import OverallState
 from src.gameMode       import GameMode
+from threading          import Thread, Lock
 
 class Client:
-
+    
     def __init__(self,server_ip,server_port,velocityBullets,regenBullets,maxBullets,balloonSeed,balloonCount):
         self.eventQueue     = []
+        self.qMutex         = Lock()
+        self.sMutex         = Lock()
         self.networkManager = NetworkClient(server_ip,server_port,self)
         self.state          = OverallState(GameMode(regenBullets, maxBullets, balloonSeed, balloonCount))
  
     def rotateClock(self):
-        self.eventQueue.append([time(),'C'])
-    
+        self.qMutex.acquire()
+        try:
+            self.eventQueue.append([time(),'C'])
+        finally:
+            self.qMutex.release()
+
     def rotateAntiClock(self):
-        self.eventQueue.append([time(),'A'])
+        self.qMutex.acquire()
+        try:
+            self.eventQueue.append([time(),'A'])
+        finally:
+            self.qMutex.release()
 
     def getState():
-        # TODO clear Queue after doing this 
-        return {x.getState() for x in self.eventQueue}
+        self.qMutex.acquire()
+        try:
+            ret = {x.getState() for x in self.eventQueue}
+            self.eventQueue = []
+        finally:
+            self.qMutex.release()
+            return ret 
 
     def setState(self,state):
-        self.OverallState.setState(state)
+        self.sMutex.acquire()
+        try:
+            self.OverallState.setState(state)   
+        finally:
+            self.sMutex.release()
