f3cb02104a770422f780a79a503c6c2ed20d2383
pushpit-sri <sonika@Sonikas-MacBook-Air-2.local>
2022-10-14 07:06:01 +0530

    added Balloon.copy()

--- src/balloonState.py
+++ src/balloonState.py
@@ -4,3 +4,6 @@ from constants import *
 class Balloon(Circle):
     def __init__(self,center,otime):
         Circle.__init__(self,center,[0,0],BALLOON_RADIUS,otime)
+
+    def copy(self):
+    	return Balloon(**self.getState())
