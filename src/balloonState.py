f4d8d2f5c1e784dca92b81b1d7bfb24bc3115598
pushpit-sri <sonika@Sonikas-MacBook-Air-2.local>
2022-10-15 06:19:59 +0530

    Added BalloonState doc

--- src/balloonState.py
+++ src/balloonState.py
@@ -2,8 +2,19 @@ from geometry import *
 from constants import *
 
 class Balloon(Circle):
+	"""
+	Used to create a Balloon object from using Circle class
+	"""
     def __init__(self,center,otime,**kwargs):
+    	"""
+		Args:
+				center (List<float>) : Coordinates of Center of balloon
+				otime (float) : Origin time of the balloon
+    	"""
         Circle.__init__(self,center,[0,0],BALLOON_RADIUS,otime)
 
     def copy(self):
+    	"""
+		Used to return a copy of this object
+    	"""
     	return Balloon(**self.getState())
