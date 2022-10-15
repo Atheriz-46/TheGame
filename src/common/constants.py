bf208505d6d5ea402b94c705accb3c42a0280385
Atheriz-46 <atherissummers@gmail.com>
2022-10-15 12:50:24 +0530

    SOcket server blunder fixed

--- src/common/constants.py
+++ src/common/constants.py
@@ -34,5 +34,6 @@ GUN_SIZE = 7
 GUN_WIDTH = 4
 N_PLAYERS = 2
 IP = "127.0.0.1"
-port = 17176
+port = 17171
 print(f"Port number is {port}")
+FIXED_SIZE = 1024
--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,5 +1,10 @@
 from .Server import Server
 from ..common.constants import IP, port
-
+import argparse
+parser = argparse.ArgumentParser(description='Start a server')
+parser.add_argument('--ip', type=str, default=IP, help='IP address of the server')
+parser.add_argument('--port', type=int, default=port, help='Port of the server')
+args = parser.parse_args()
 print("Server started")
-Server(ip=IP, port=port)
+Server(ip=args.ip, port=args.port)
+
