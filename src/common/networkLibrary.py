e6135fe666bab8176862214c31776c14d4a74c00
HrshWardhan <hwardhan337@gmail.com>
2022-10-15 13:38:34 +0530

    test network

--- src/common/networkLibrary.py
+++ src/common/networkLibrary.py
@@ -1,44 +1,58 @@
 from .constants import FIXED_SIZE
-def sendMessage(conn, s):
-    """
-    Used to send message s using Socket conn.
-    Handles underflow and overflow of messages.
-    Acts as a middle layer between networks buffers and higher level functions.
+from time import sleep
+from threading import Thread,Lock
+class messenger:
+    def __init__(self):
+        self.buffer = []
+        self.messageBuffer = []
+    def sendMessage(conn, s):
+        """
+        Used to send message s using Socket conn.
+        Handles underflow and overflow of messages.
+        Acts as a middle layer between networks buffers and higher level functions.
 
-    Args:
-            conn (Socket) : Socket over which message is to be sent
-            s (str) : Message to be sent
-    """
+        Args:
+                conn (Socket) : Socket over which message is to be sent
+                s (str) : Message to be sent
+        """
 
-    s += "%"
-    p = ""
-    for i in s:
-        p += i
-        if len(p.encode("utf-16")) >= FIXED_SIZE:
+        s += "%"
+        for i in s:
+            p += i
+            if len(p.encode("utf-16")) >= FIXED_SIZE:
+                conn.send(p.encode("utf-16"))
+                p = ""
+        if len(p):
             conn.send(p.encode("utf-16"))
-            print(p.encode("utf-16"))
-            p = ""
-    if len(p) == 0:
-        pass
-    else:
-        while len(p.encode("utf-16")) < FIXED_SIZE:
-            p += "*"
-        conn.send(p.encode("utf-16"))
-        print(p.encode("utf-16"))
 
 
-def recieveMessage(conn):
-    """
-    Used to receive messages over Socket conn
+    def recieveMessage(self,conn):
+        """
+        Used to receive messages over Socket conn
 
-    Args:
-            conn (Socket) : Socket over which messages are to be received
-    """
-    s = ""
-    while True:
-        curr = conn.recv(FIXED_SIZE).decode("utf-16")
-        s += curr
-        if "%" in s:
-            break
-    print(s)
-    return s.split("%")[0]
+        Args:
+                conn (Socket) : Socket over which messages are to be received
+        """
+        s = ""
+        while True:
+            self.rlock.acquire()
+            if len(self.messageBuffer):
+                p = self.messageBuffer[0]
+                messageBuffer.pop(0)
+                self.rlock.release()
+                return p 
+            else:
+                self.rlock.release()
+                sleep(0.05)
+
+            
+    def reader(self,conn):
+        while True:
+            curr = conn.recv(FIXED_SIZE).decode("utf-16") 
+            for i in curr:
+                if i == '%':
+                    self.rlock.acquire()
+                    self.messageBuffer.append("".join(self.buffer))
+                    self.rlock.release()
+                else:
+                    self.buffer.append(i)
